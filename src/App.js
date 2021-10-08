edf74d90c13f38fa5c8ba845f1a14983d692d57e
Behar Kadri <kadri.behar@gmail.com>
2021-10-08 23:26:31 +0200

    Created header and list of available meals components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Meals />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,22 @@
+import MealItemForm from './MealItemForm';
+
+import classes from './MealItem.module.css';
+
+const MealItem = ({ name, description, price }) => {
+  const formattedPrice = `$${price.toFixed(2)}`;
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{name}</h3>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>{formattedPrice}</div>
+      </div>
+      <div>
+        <MealItemForm />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
